340ddbbe11fae5191a280ccc91b349309d5e7cb9
sheabali <iamskpranto@gmail.com>
2025-04-13 23:50:49 +0600

    work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center text-6xl mx-auto mt-80">
-      Meal Box
+      Meal Box Project
     </div>
   );
 }

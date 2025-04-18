9b7a23d7c7842d27e4f025b93ed9807772931e86
sheabali <iamskpranto@gmail.com>
2025-04-18 20:17:06 +0600

    create dashboard layout

---
+++ src/components/modules/dashboard/sidebar/app-sidebar.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import * as React from 'react';
+import {
+  Bot,
+  GalleryVerticalEnd,
+  LifeBuoy,
+  Send,
+  Settings,
+  SquareTerminal,
+} from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from '@/components/ui/sidebar';
+import { TeamSwitcher } from './team-switcher';
+import { NavMain } from './nav-main';
+import { NavUser } from './nav-user';
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '/user/dashboard',
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: 'Shop',
+      url: '/user/shop/products',
+      icon: Bot,
+      items: [
+        {
+          title: 'Manage Products',
+          url: '/user/shop/products',
+        },
+        {
+          title: 'Manage Categories',
+          url: '/user/shop/category',
+        },
+        {
+          title: 'Manage Coupon',
+          url: '/user/shop/manage-coupon',
+        },
+        {
+          title: 'Manage Brands',
+          url: '/user/shop/brand',
+        },
+      ],
+    },
+
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings,
+      items: [
+        {
+          title: 'Profile',
+          url: '/profile',
+        },
+      ],
+    },
+  ],
+  teams: [
+    {
+      name: 'Acme Inc',
+      logo: GalleryVerticalEnd,
+      plan: 'Enterprise',
+    },
+  ],
+  navSecondary: [
+    {
+      title: 'Support',
+      url: '#',
+      icon: LifeBuoy,
+    },
+    {
+      title: 'Feedback',
+      url: '#',
+      icon: Send,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
